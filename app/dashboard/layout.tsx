28ba8355c6810c283ce6df6ee6a544028bb3fd0d
Aleksandr Dosaev <alexander.dosaev@gmail.com>
2024-01-17 12:46:18 +0400

    first com

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+import SideNav from '@/app/ui/dashboard/sidenav';
+import { inter } from '../ui/fonts';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div
+        className={`${inter.className} flex-grow p-6 antialiased md:overflow-y-auto md:p-12`}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
